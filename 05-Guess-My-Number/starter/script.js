a6ca45a16cbb9201ff96761e957d681df50ea8e8
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-08-15 13:54:07 -0700

    Guess Number Game is Done

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,56 @@
 'use strict';
+
+document.querySelector('.message').textContent;
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+document.querySelector('.number').textContent = '?';
+document.querySelector('.score').textContent = 20;
+let number = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayScore = function (score) {
+    document.querySelector('.score').textContent = score;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(number);
+  if (guess == number) {
+    document.body.style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = number;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+  } else if (!guess) {
+    displayMessage('⛔️ No number!');
+  } else if (guess !== number) { 
+    if (score > 0) {
+      score--;
+      displayScore(score);
+      displayMessage(guess > number ? '📈 Too High!' : '📉 Too Low!');
+    } else {
+      displayMessage('💥 You lost the game!');
+      displayScore() = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  number = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  displayScore(score);
+  displayMessage('Start guessing...');
+  displayScore(score);
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.body.style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});

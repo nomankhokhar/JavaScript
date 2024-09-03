5d06e826924f401cf0a97526dbe9efaef9f603f6
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-03 23:57:48 +0500

    Function in Depth Start

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,107 @@
 'use strict';
+
+// -> Default Parameters
+
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
+
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 5);
+// createBooking('LH123', undefined, 1000);
+
+// console.log(bookings);
+
+// -> Passing Arguments: Value vs Reference
+
+// const flight = 'LH234'; // Primitive value pass by value
+// const NomanAli = {
+//   // Non Primitive Object pass by reference
+//   name: 'Noman Ali',
+//   passport: 24739479284,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 24739479284) {
+//     alert('Check in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
+
+// checkIn(flight, NomanAli);
+// console.log(flight);
+// console.log(NomanAli);
+
+// // Is the same as doing...
+// const flightNum = flight;
+// const passenger = NomanAli;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 10000000000);
+// };
+
+// newPassport(NomanAli);
+// checkIn(flight, NomanAli);
+
+// -> Higher-Order Functions / Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original String: ${str}`);
+  console.log(`Transformed String: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`); // Function name property that you can use pass in transformer function
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+
+['Noman', 'Ali', 'Nomi'].forEach(high5);
+
+// -> Functions Returning Functions
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('Noman');
+greeterHey('Ali');

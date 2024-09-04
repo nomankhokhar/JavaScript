9be0f9aa694b08c9fab2c5c3cf3c02916adbc0c6
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-05 02:16:09 +0500

    Building the Bankist App

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -63,7 +63,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// LECTURES
 
 const currencies = new Map([
   ['USD', 'United States dollar'],
@@ -72,6 +71,25 @@ const currencies = new Map([
 ]);
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}"> ${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}€</div>
+      </div>
+      `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
 
 /////////////////////////////////////////////////
 
@@ -160,3 +178,5 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // currentSet.forEach(function (value, _, set) {
 //   console.log(`${value}: ${(value, _)}`);
 // });
+
+// -> Project: Bankist App

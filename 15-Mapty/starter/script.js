6afdd5fd2139417a4885e88ce6acf5ab44b143f6
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-17 01:11:34 +0500

    Mapty

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,42 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// -> How to plan a Web Project
+
+// 1 - User Stories
+// 2 - Features
+// 3 - Flowchart
+// 4 - Architecture
+
+// 5 - Development
+// 6 - Testing
+// 7 - Deployment
+// 8 - Maintenance
+
+// -> Using a Geolocation API
+
+// 1 - Get user's current position
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+
+    const coords = [latitude, longitude];
+
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  function () {
+    alert('Could not get your position');
+  }
+);

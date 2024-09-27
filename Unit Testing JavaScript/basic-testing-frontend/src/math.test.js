ab344172e6030add210bed9139ebe1beca8bc693
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-27 23:50:31 +0500

    Basic Understanding of Unit testing is Inevitable

--- Unit Testing JavaScript/basic-testing-frontend/src/math.test.js
+++ Unit Testing JavaScript/basic-testing-frontend/src/math.test.js
@@ -1,7 +1,24 @@
 import { expect, it } from "vitest";
 import { add } from "./math";
 
+// AAA Pattern
+
+// 1 Arrange: Setting up the values
+// 2 Act:   Acting on the values
+// 3 Assert: Asserting the values of that result
+
 it("should summarize all number values in an array", () => {
-  const result = add([1, 2, 3, 4, 5]);
-  expect(result).toBe(15);
+  // Arranging the values
+  const numbers = [1, 2, 3, 4, 5];
+
+  // Acting on the values
+  const result = add(numbers);
+
+  // Asserting the values
+  const expectedValues = numbers.reduce((acc, value) => acc + value, 0);
+  expect(result).toBe(expectedValues);
+});
+
+it("should yield NaN if a least one invalid number is provided", () => {
+  const inputs = ["input", 1];
 });

fd3b7e14cd258e93f57bfeee6803bd144432777c
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-09-27 19:14:09 +0500

    Setup

---
+++ Unit Testing JavaScript/2 Setup&Testing.ts
@@ -0,0 +1,14 @@
+// There are many test cases
+
+// Jest is a popular testing framework for JavaScript (Test Runner)
+// Runs your test
+// Automatically detects testing code
+// Displays results in the console
+// Jest and Karma are popular testing frameworks
+
+// Assertion Library - Jest uses Jest Expect
+
+// Used to define expected output
+// Checks whether expectations are met
+// Supports all kinds of exceptional and modes
+// e.g, Jest Check, etc ...

9fc4ad7cea77107df7f2b189bf3baffc108971cb
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-10-13 03:44:36 +0500

    Object and Array testing

--- JavaScript Unit Testing/javascript-testing/tests/core.test.js
+++ JavaScript Unit Testing/javascript-testing/tests/core.test.js
@@ -1,4 +1,5 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
+import { getCoupons } from "../src/core";
 
 // Use toBe for primitive types (numbers, strings, booleans, etc.) where strict equality matters.
 // Use toEqual for comparing objects, arrays, or other complex types where you want to check for deep equality.
@@ -7,5 +8,39 @@ describe("test suite", () => {
   it("test case", () => {
     const result = { name: "Noman Ali" };
     expect(result).toEqual({ name: "Noman Ali" });
+
+    const res = "The requested file was not found!";
+    // Loose (too general)
+    expect(res).toBeDefined();
+
+    // Tight (too specific)
+    // expect(res).toBe("The requested file was not found.");
+
+    // Better Assertion
+    expect(res).toMatch(/not found/i);
+
+    const resArr = [1, 2, 3];
+
+    // loose Assertion
+    expect(resArr).toBeDefined;
+
+    // Tight Assertion
+    expect(resArr).toEqual(expect.arrayContaining([2, 1, 3]));
+
+    // can be Check at length
+    expect(resArr).toHaveLength(3);
+
+    const resObj = { name: "Noman Ali", id: 1 };
+
+    expect(resObj).toMatchObject({ name: "Noman Ali" });
+    expect(resObj).toHaveProperty("name");
+    expect(typeof resObj.name).toBe("string");
+  });
+});
+
+describe("getCoupens", () => {
+  it("should return an array of coupons", () => {
+    const coupons = getCoupons();
+    expect(coupons.length).toBeGreaterThan(0);
   });
 });
